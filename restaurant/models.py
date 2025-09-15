6a829a3f4698c95a34ca68b3cc1d18adfcf750c0
KimDongSu <kimdongsu481@gmail.com>
2025-09-16 00:08:35 +0900

    도커 직전까지 완성

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="restaurants/")
+    image = models.CharField()
     context = models.TextField()
     naver_link = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
